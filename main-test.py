1991fb79416aa642aec1a8c275a7a04ed992c0a1
trejo24 <109001098+trejo24@users.noreply.github.com>
2022-08-01 14:03:46 -0500

    Add files via upload

---
+++ main-test.py
@@ -0,0 +1,9 @@
+# Importe la función Agregar y afirme que funciona correctamente. 
+desde la importación principal Añadir
+
+def TestAdd():
+         afirmar Add(2,3) == 5
+        print( "Añadir función funciona correctamente" )
+
+si __nombre__ == '__principal__' :
+        PruebaAñadir()
---
+++ main.py
@@ -0,0 +1,8 @@
+def Añadir(a, b):
+         devuelve a + b
+        
+def Saluda():
+        print( "buen mundo de srcmake" )
+
+si __nombre__ == '__principal__' :
+        Di hola()
